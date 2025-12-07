8afd439a55b62c3d5433ec1fe8d22835947b4fb7
pratheist <pratheist@proton.me>
2025-12-07 22:16:25 +0530

    Fixed path for utils.tx

--- src/components/ui/gradient-button.tsx
+++ src/components/ui/gradient-button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "/Users/prateekkumud/Documents/Riyaz/sqtesting/sqt/src/lib/utils.ts"
+import { cn } from "/src/lib/utils.ts"
 
 const gradientButtonVariants = cva(
   [
